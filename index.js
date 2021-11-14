53f7e66688c4d5a4a05bba394e59cf88bab118fd
isoumpasis <wweacountss@gmail.com>
2021-11-14 22:28:48 +0200

    slug test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 1000;
 
-app.use(express.static('.'));
+app.use('/slug', express.static('.'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
